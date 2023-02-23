e1d8c202de9c7f079baa1e171d83ca8c3aed9ca9
prosas <prosas@dealergeek.com>
2023-02-23 10:11:31 -0600

    fix(deploy): fix scrip deploy

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,9 @@
+killall docker
 systemctl stop nginx
 git pull origin master
+docker run -p 3000:3000 -d sicei:0.0.1
 systemctl start nginx
-ngrok http 80 >> /dev/null &
+ngrok http 3000 >> /dev/null &
 url=$(curl -s localhost:4040/api/tunnels | jq .tunnels[0].public_url)
 echo "Application served in $url"
 
