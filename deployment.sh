0522dbd1846968c18c79fb5a556e309aed1bcc8f
prosas <prosas@dealergeek.com>
2023-02-23 10:41:07 -0600

    fix typo in deployment script3

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,9 @@
 killall ngrok 
 killall docker
+docker stop $(docker ps -a -q)
 systemctl stop nginx
 git pull origin main
-docker run -p 3000:3000 -d sicei:0.0.1
+docker build . -t sicei:0.0.1 && docker run -p 3000:3000 -d sicei:0.0.1
 systemctl start nginx
 ngrok http 3000 >> /dev/null &
 url=$(curl -s localhost:4040/api/tunnels | jq .tunnels[0].public_url)
