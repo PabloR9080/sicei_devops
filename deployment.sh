3927b9bdfca5db1ecd7ab98fbfac9d406ec9d05c
prosas <prosas@dealergeek.com>
2023-02-23 13:09:50 -0600

    add sleep

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,13 @@
+#!bin/bash
+
 killall ngrok 
 killall docker
 docker stop $(docker ps -a -q)
-systemctl stop nginx
 git pull origin main
-docker build . -t sicei:0.0.1 && docker run -p 3000:3000 -d sicei:0.0.1
-systemctl start nginx
+docker build . -t sicei:0.0.1
+docker run -p 3000:3000 -d sicei:0.0.1
 ngrok http 3000 >> /dev/null &
+sleep 5
 url=$(curl -s localhost:4040/api/tunnels | jq .tunnels[0].public_url)
-echo "Application served in $url"
+echo "Application served in $url" 
 
