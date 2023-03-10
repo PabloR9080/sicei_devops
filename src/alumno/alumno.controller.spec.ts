8f9cf469c6e792d142f8818fa5410625694f24b1
Pablo Rosas <pablo@MacBook-Air-de-Pablo.local>
2023-03-10 09:15:05 -0600

    unit tests

--- src/alumno/alumno.controller.spec.ts
+++ src/alumno/alumno.controller.spec.ts
@@ -4,6 +4,7 @@ import { AlumnoService } from './alumno.service';
 
 describe('AlumnoController', () => {
   let controller: AlumnoController;
+  let service: AlumnoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,18 @@ describe('AlumnoController', () => {
     }).compile();
 
     controller = module.get<AlumnoController>(AlumnoController);
+    service = module.get<AlumnoService>(AlumnoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('shoyld call the service', async () => {
+    const result = [{ id: 1, nombre: 'Juan' , apellido: 'Perez'}];
+    jest.spyOn(service, 'findAll').mockImplementation(() =>  result ); 
+
+    expect(await controller.findAll()).toBe(result);
+
   });
 });
